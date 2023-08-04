1d50e6b2e3dbd09a2657e9f7f3a6fb5c3bc4b183
vlj2007 <vlj2007@yandex.ru>
2023-08-05 00:36:48 +0700

    I_have_finished_task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
-    public static int[] generateRandomArray () {
+
+    public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
@@ -10,15 +12,36 @@ public class Main {
         }
         return arr;
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         int[] arr = generateRandomArray();
-        int costs =0;
-        for (int index = 0; index < arr.length -1; index++){
+        int costs = 0;
+        for (int index = 0; index < arr.length - 1; index++) {
             costs += arr[index];
+            System.out.println("Сумма трат за месяц составила " + costs + " рублей");
         }
-        System.out.println("Сумма трат за месяц составила " + costs + " рублей");
+//        System.out.println("Сумма трат за месяц составила " + costs + " рублей");
     }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
 
+        int mixWaste = 200_201;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < mixWaste) {
+                mixWaste = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + mixWaste + " рублей");
+
+        int maxWaste = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxWaste) {
+                maxWaste = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxWaste + " рублей");
+    }
 }
