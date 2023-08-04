23e9e8e5cda066426b66e206eea57a14f7db90c1
vlj2007 <vlj2007@yandex.ru>
2023-08-04 23:46:59 +0700

    I_have_finished_task1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,23 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
-    public static void task1(){
-        System.out.println("Задача 1");
-
+    public static int[] generateRandomArray () {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int costs =0;
+        for (int index = 0; index < arr.length -1; index++){
+            costs += arr[index];
+        }
+        System.out.println("Сумма трат за месяц составила " + costs + " рублей");
+    }
+
+
 }
